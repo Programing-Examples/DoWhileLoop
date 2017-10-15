8ee7bd12b43483b275f5d1fa10aa2e85ee6f3482
01omar10 <omardiaz88@gmail.com>
2017-10-16 00:20:08 +0200

    Simple example of how do-while structure works

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,12 @@
+package main.java.com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int value = 1;
+
+	    do {
+            System.out.println("El programa se ejecuta al menos una vez, incluso si la condición no se cumple");
+        }while (value == 0);
+    }
+}
